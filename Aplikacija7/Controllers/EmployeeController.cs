e492c62c851c521c9ae3845fb3604515da7ee7ed
marijadamnjanovic <marijaamnjanovic92@gmail.com>
2015-08-10 11:20:08 +0200

    App

---
+++ Aplikacija7/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aplikacija7.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeContext db = new EmployeeContext();
+
+        public ActionResult Index() {
+
+
+
+            return View(db.Employees.ToList());
+
+        }
+    }
+}
---
+++ Aplikacija7/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Aplikacija7.Startup))]
+namespace Aplikacija7
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
